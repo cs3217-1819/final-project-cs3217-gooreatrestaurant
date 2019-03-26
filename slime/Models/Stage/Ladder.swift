9a5782d133cd04d1513bb50020f57c8e9ffe2b47
Hui Qi <e0007393@u.nus.edu>
2019-03-27 02:11:02 +0800

    Set up ladder physics

--- slime/Models/Stage/Ladder.swift
+++ slime/Models/Stage/Ladder.swift
@@ -16,27 +16,13 @@ class Ladder: SKSpriteNode {
         super.init(texture: ladder, color: .clear, size: StageConstants.ladderSize)
         self.position = position
         self.zPosition = 2
+
+        self.physicsBody = SKPhysicsBody(rectangleOf: CGSize(width: 10, height: 45))
+        self.physicsBody?.categoryBitMask = StageConstants.ladderCategory
+        self.physicsBody?.isDynamic = false
     }
 
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-
-//    func buildLadder(position: CGPoint) {
-//        let ladder = SKTexture(imageNamed: "Ladder")
-//        ladder.filteringMode = .nearest // shorter form for SKTextureFilteringMode.Nearest
-//        spaceship = SKSpriteNode(texture: ladder)
-//        spaceship.position = position
-//        spaceship.setScale(0.1)
-//        spaceship.zPosition = 2
-//        addChild(spaceship)
-//
-//        let ladderBody = SKNode()
-//        ladderBody.position = position
-//        ladderBody.name = "LadderBody"
-//        ladderBody.physicsBody = SKPhysicsBody(rectangleOf: CGSize(width: 10, height: 45))
-//        ladderBody.physicsBody?.categoryBitMask = interactableObjCategory
-//        ladderBody.physicsBody?.isDynamic = false
-//        self.addChild(ladderBody)
-//    }
 }
