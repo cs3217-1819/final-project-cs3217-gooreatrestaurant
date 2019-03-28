b7dc53dcc66db660889a320f9f5bd618c9372ef6
Maxheart <samuelhenrykurniawan@yahoo.com>
2019-03-28 23:50:57 +0800

    implement table

--- slime/Models/Stage/Station/Table.swift
+++ slime/Models/Stage/Station/Table.swift
@@ -9,6 +9,37 @@
 import UIKit
 import SpriteKit
 
-class Table: SKSpriteNode {
+class Table: Station {
+
+    override init(inPosition position: CGPoint, withSize size: CGSize = StageConstants.stationSize) {
+        super.init(inPosition: position, withSize: size)
+        self.color = .yellow
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func ableToProcess(_ item: SKSpriteNode?) -> Bool {
+        if item == nil {
+            return false
+        }
+        return true
+    }
+
+    override func process(_ item: SKSpriteNode?) -> SKSpriteNode? {
+        guard ableToProcess(item) == true else {
+            return nil
+        }
+
+        guard let itemToPut = item else {
+            return item
+        }
+
+        itemToPut.removeFromParent()
+        itemToPut.position = CGPoint(x: 0.0, y: 0.5 * (itemToPut.size.height + self.size.height))
+        addChild(itemToPut)
+        return nil
+    }
 
 }
