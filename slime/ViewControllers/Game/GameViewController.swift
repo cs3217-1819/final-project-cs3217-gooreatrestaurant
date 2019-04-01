09da0d0db3164ab9e01787c864a9bacf8238867c
Hui Qi <e0007393@u.nus.edu>
2019-04-01 23:01:51 +0800

    Set up temp collection view

--- slime/ViewControllers/Game/GameViewController.swift
+++ slime/ViewControllers/Game/GameViewController.swift
@@ -9,7 +9,22 @@
 import UIKit
 import SpriteKit
 
-class GameViewController: UIViewController {
+class GameViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 2
+    }
+
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let myCell = collectionView.dequeueReusableCell(withReuseIdentifier: "MyCell", for: indexPath as IndexPath)
+        myCell.backgroundColor = UIColor.blue
+        return myCell
+    }
+
+    func collectionView(collectionView: UICollectionView, didSelectItemAtIndexPath indexPath: IndexPath)
+    {
+        print("User tapped on item \(indexPath.row)")
+    }
+
    override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
 
@@ -25,6 +40,17 @@ class GameViewController: UIViewController {
         view.addSubview(skview)
 
         stage.generateLevel(inLevel: "Level1")
+
+        let layout: UICollectionViewFlowLayout = UICollectionViewFlowLayout()
+        layout.sectionInset = UIEdgeInsets(top: 20, left: 10, bottom: 10, right: 10)
+        layout.itemSize = CGSize(width: 60, height: 60)
+
+        let myCollectionView:UICollectionView = UICollectionView(frame: self.view.frame, collectionViewLayout: layout)
+        myCollectionView.dataSource = self
+        myCollectionView.delegate = self
+        myCollectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "MyCell")
+        myCollectionView.backgroundColor = UIColor.clear
+        self.view.addSubview(myCollectionView)
     }
 
     lazy var skView: SKView = {
