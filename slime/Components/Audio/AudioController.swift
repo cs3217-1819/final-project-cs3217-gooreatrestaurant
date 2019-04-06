1e589ea8d39867c75ef64ac80a4cab0562407db4
Hui Qi <e0007393@u.nus.edu>
2019-04-06 16:42:23 +0800

    Set up audio script

---
+++ slime/Components/Audio/AudioController.swift
@@ -0,0 +1,28 @@
+//
+//  AudioController.swift
+//  slime
+//
+//  Created by Developer on 6/4/19.
+//  Copyright © 2019 nus.cs3217.a0166733y. All rights reserved.
+//
+
+import Foundation
+import AVFoundation
+
+class AudioController {
+    private(set) var music: AVAudioPlayer!
+
+    func playMusic(_ audioName: String, _ ifLoop: Bool) {
+        do {
+            music = try AVAudioPlayer(contentsOf: URL.init(fileURLWithPath:
+                Bundle.main.path(forResource: audioName, ofType: "mp3")!))
+            music.numberOfLoops = ifLoop ? -1 : 0
+            music.play()
+        } catch {
+            print("Error loading Music")
+        }
+    }
+    func stopMusic() {
+        music.stop()
+    }
+}
