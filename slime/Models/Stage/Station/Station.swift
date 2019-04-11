e9dc14bb37b2e9a7c73536700a5541dff0a96565
Maxheart <samuelhenrykurniawan@yahoo.com>
2019-04-12 01:15:35 +0800

    write comments on Station class

--- slime/Models/Stage/Station/Station.swift
+++ slime/Models/Stage/Station/Station.swift
@@ -11,9 +11,17 @@ import SpriteKit
 
 class Station: SKSpriteNode {
 
+    // ID of the station, to refer to it in the networking database
     var id: String?
 
+    // Construct a station
+    // The station will be able to have contact with slime object, but not colliding with it
+    // Parameters:
+    //      - position: the position of the center of mass of the station relative to the parent
+    //      - size: the size of the station, default value is located in the constants
     init(inPosition position: CGPoint, withSize size: CGSize = StageConstants.stationSize) {
+
+        // default texture of station is using table's texture
         let table = SKSpriteNode(imageNamed: "table")
         table.size = size
         super.init(texture: table.texture, color: .clear, size: size)
@@ -32,10 +40,20 @@ class Station: SKSpriteNode {
         fatalError("init(coder:) has not been implemented")
     }
 
+    // Check whether the item is able to be processed by this station
+    // Parameters:
+    //      - item: the item that is queried, can be nil, where there is no item passed to this station
+    // Return value:
+    //        true if the the item is able to be processed by this station, false otherwise
     func ableToProcess(_ item: SKSpriteNode?) -> Bool {
         return false
     }
 
+    // Process an item
+    // Parameter:
+    //      - item: the item that will be processed, can be nil when there is no item passed to this station
+    // Return value:
+    //      Optional SKSpriteNode, the item that has been processed (or nil if the processing does not produce anything)
     func process(_ item: SKSpriteNode?) -> SKSpriteNode? {
         return nil
     }
