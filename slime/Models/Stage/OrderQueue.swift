6f1406fd6a5d0dbc960ff52879e1d8b324c09e2b
Maxheart <samuelhenrykurniawan@yahoo.com>
2019-04-21 20:14:11 +0800

    add comments

--- slime/Models/Stage/OrderQueue.swift
+++ slime/Models/Stage/OrderQueue.swift
@@ -47,49 +47,12 @@ class OrderQueue: SKSpriteNode, Codable {
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-    
-    func setMultiplayer(withGameId id: String) {
-        self.isMultiplayerEnabled = true
-        self.gameId = id
-    }
 
-    private func addOrderFronDecoder(ofRecipe recipe: Recipe, andPrefab prefab: MenuPrefab) {
-        recipeOrdered.append(recipe)
-        nodeOrder.append(prefab)
-        self.addChild(prefab)
-    }
-
-    func generateMenu(ofRecipe recipe: Recipe) {
-        let menuObj = MenuPrefab(color: StageConstants.menuPrefabColor, size: StageConstants.menuPrefabSize)
-        menuObj.addRecipe(recipe, inPosition: positionings[recipeOrdered.count - 1])
-        nodeOrder.append(menuObj)
-        self.addChild(menuObj)
-    }
-
-    func addOrder(ofRecipe recipe: Recipe) {
-        guard self.recipeOrdered.count < StageConstants.maxNumbersOfOrdersShown else {
-            return
-        }
-        recipeOrdered.append(recipe)
-        generateMenu(ofRecipe: recipe)
-    }
-
-    func generateRandomRecipe() -> Recipe? {
-        return self.possibleRecipes.randomElement()?.generateRecipe()
-    }
-
-    @objc
-    func addRandomOrder() {
-        guard let randomRecipe = self.generateRandomRecipe() else {
-            return
-        }
-        self.addOrder(ofRecipe: randomRecipe)
-    }
-    
     func addPossibleRecipe(_ template: RecipeTemplate) {
         self.possibleRecipes.insert(template)
     }
-    
+
+    // For the new order timer
     private func generateTimer() {
         newOrderTimer = Timer.scheduledTimer(withTimeInterval: interval, repeats: false, block: { _ in
             if (self.orderQueueInvalidated) {
@@ -101,6 +64,7 @@ class OrderQueue: SKSpriteNode, Codable {
         })
     }
 
+    // generate timer, then the first few orders
     func initialize() {
         guard !possibleRecipes.isEmpty else {
             return
@@ -115,6 +79,48 @@ class OrderQueue: SKSpriteNode, Codable {
         
         if isMultiplayerEnabled { multiplayerUpdateSelf() }
     }
+
+    func setMultiplayer(withGameId id: String) {
+        self.isMultiplayerEnabled = true
+        self.gameId = id
+    }
+
+    private func addOrderFronDecoder(ofRecipe recipe: Recipe, andPrefab prefab: MenuPrefab) {
+        recipeOrdered.append(recipe)
+        nodeOrder.append(prefab)
+        self.addChild(prefab)
+    }
+
+    // for UI in the right side
+    func generateMenu(ofRecipe recipe: Recipe) {
+        let menuObj = MenuPrefab(color: StageConstants.menuPrefabColor, size: StageConstants.menuPrefabSize)
+        menuObj.addRecipe(recipe, inPosition: positionings[recipeOrdered.count - 1])
+        nodeOrder.append(menuObj)
+        self.addChild(menuObj)
+    }
+
+    // general function to add order
+    func addOrder(ofRecipe recipe: Recipe) {
+        guard self.recipeOrdered.count < StageConstants.maxNumbersOfOrdersShown else {
+            return
+        }
+        recipeOrdered.append(recipe)
+        generateMenu(ofRecipe: recipe)
+    }
+
+    func generateRandomRecipe() -> Recipe? {
+        return self.possibleRecipes.randomElement()?.generateRecipe()
+    }
+
+    // generate random recipe, then add order of that recipe
+    @objc
+    func addRandomOrder() {
+        guard let randomRecipe = self.generateRandomRecipe() else {
+            return
+        }
+        self.addOrder(ofRecipe: randomRecipe)
+    }
+
 
     // True if success, false if failed (no corresponding orders)
     func completeOrder(withFood food: Food) -> Bool {
@@ -146,7 +152,7 @@ class OrderQueue: SKSpriteNode, Codable {
         self.setMultiplayerTimeout(forRecipe: recipe)
     }
 
-    func removeMenuPrefab(inNum: Int) {
+    private func removeMenuPrefab(inNum: Int) {
         //remove the node image and the list
         nodeOrder[inNum].removeFromParent()
         let node = nodeOrder.remove(at: inNum)
@@ -168,6 +174,7 @@ class OrderQueue: SKSpriteNode, Codable {
         return score
     }
 
+    // For multiplayer
     enum CodingKeys: String, CodingKey {
         case possibleRecipes
         case recipeOrdered
